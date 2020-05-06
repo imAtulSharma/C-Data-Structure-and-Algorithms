22b258a17050f7a460accc903e8b1ee576b0a1f4
Atul Sharma <atulsharma20may@outlook.com>
2020-05-06 18:33:43 +0530

    To upload files

---
+++ 7-Inserting node in ending of list.c
@@ -0,0 +1,125 @@
+// Simply inserting element in the ending of a linked list
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node *start;
+
+struct node
+{
+    int roll;
+    struct node *next;
+};
+
+struct node * create_linked_list(struct node *);
+void display(struct node *);
+void insert_end(struct node *);
+
+
+void main()
+{    
+    int roll;
+    
+    printf("Enter negative number to stop\n");
+    printf("Enter roll number\n");
+    scanf("%d", &roll);
+    
+    if(roll < 0)
+    {
+        printf("You have canceled\n");
+    }
+    
+    else
+    {   
+        start = (struct node *) malloc(sizeof(struct node));
+
+        if(start == NULL)
+        {
+            printf("Unable to make linked list due to no space in heap\n");//When there is no free space in heap is left for memory allocation
+        }
+        else
+        {
+            start -> roll = roll;
+            create_linked_list(start);
+            display(start);
+
+            printf("\nThe modified linked list is the following...");
+            insert_end(start);
+            display(start);
+        }
+    }
+}
+
+struct node * create_linked_list(struct node *ptr)
+{
+    struct node *new_node;
+    static int count = 1, roll;        
+        
+    printf("Enter negative number to stop\n");
+    printf("Enter roll number\n");
+    scanf("%d", &roll);
+            
+    if (roll < 0)//if user not want to create list, so here we don not waste any kind of memory
+    {
+
+        goto End;
+    }
+
+    else
+    {
+        new_node = (struct node *) malloc(sizeof(struct node));
+
+        if(new_node == NULL)
+        {
+            printf("No more space in heap\n");//When there is no free space in heap is left for memory allocation
+                
+            goto End;
+        }
+
+        else
+        {
+            ptr -> next = new_node;
+            ptr = ptr -> next;
+
+            ptr -> roll = roll;
+            count++;
+            
+            return create_linked_list(ptr);
+        }
+    }
+    End:
+    ptr -> next = NULL;
+    printf("Hurray! You have made %d insertion successfully in the linked list\n", count);
+}
+
+void display(struct node *ptr)
+{
+    printf("The list is the following...\n");
+
+    do
+    {
+        printf("%d ", ptr -> roll);
+        ptr = ptr -> next;
+    }while (ptr -> next != NULL);
+
+    printf("%d ", ptr -> roll);
+}
+
+void insert_end(struct node * ptr)
+{
+    struct node * new_node = (struct node *)malloc(sizeof(struct node));
+    int roll;
+    
+    while(ptr -> next != NULL)
+    {
+        ptr = ptr -> next;
+    }
+
+    ptr -> next = new_node;
+
+    printf("\nWhich roll no you want to insert at the ending\n");
+    scanf("%d", &roll);
+
+    new_node -> roll = roll;
+    new_node -> next = NULL;
+}
