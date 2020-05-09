22b6317f477268b0b748bbeba0f24f7851fc46d5
Atul Sharma <atulsharma20may@outlook.com>
2020-05-09 15:54:45 +0530

    To upload files

---
+++ 12-Peek in stack using array.c
@@ -0,0 +1,61 @@
+// Prgramme to Peek an element in the stack using array
+
+#include<stdio.h>
+
+#define Max 7
+int stack[Max], Top = -1;
+
+int peek_in_stack(int []);
+void display_stack(int []);
+
+void main()
+{   
+    int value;
+
+    // To initialize the stack with some values
+    for (int i = 0; i < Max; i++)
+    {
+        Top++;
+        stack[i] = i+1;
+    }
+
+    display_stack(stack);
+
+    value = peek_in_stack(stack);
+
+    if(value != -1)
+    {
+        printf("The topmost element in the stack is %d", value);
+    }
+}
+
+int peek_in_stack(int stack[])
+{   
+    if(Top == -1)
+    {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    else
+    {
+        return stack[Top];
+    }
+}
+
+void display_stack(int stack[])
+{
+    if(Top == -1)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("The Stack is the following...\n[");    
+        for (int i = 0; i <= Top; i++)
+        {
+            printf("%d, ", stack[i]);
+        }
+        printf("\b\b]\n");
+    }
+    printf("The top is at %d\n", Top);
+}
